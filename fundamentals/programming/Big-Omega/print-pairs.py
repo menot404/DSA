6568f65884c3c30c1774a3104bc2d091e6744c7d
Me_Not404 <m3not404@gmail.com>
2026-04-23 09:00:47 +0000

    Doc: add comment function printPairs

--- fundamentals/programming/Big-Omega/print-pairs.py
+++ fundamentals/programming/Big-Omega/print-pairs.py
@@ -8,6 +8,21 @@ Consider an example to print all the possible pairs of an array. The idea is to 
 
 # The time complexity of this algorithm is O(n^2) because we have two nested loops, each running n times. However, we can also say that the time complexity is Ω(n^2) because in the worst case, we will have to print all the pairs, which takes n^2 time.
 """
+
+"""
+Prints all pairs of an array.
+Arguments:
+----------
+arr : list
+    The input array for which to print pairs.
+n : int
+    The size of the input array.
+Returns:
+--------
+None
+    This function does not return anything. It prints all pairs of the input array to the console
+"""
+
 def printPairs(arr, n):
     for i in range(n):
         for j in range(n):
