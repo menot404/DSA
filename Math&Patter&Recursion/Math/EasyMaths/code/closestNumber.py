7154d085d2f9f8934deadf9b9a0ca9a22afa375e
Me_Not404 <m3not404@gmail.com>
2026-05-12 15:17:48 +0000

    example of code with Closest to n and Divisible by m - [Expected Approach] By finding Quotient - O(1) Time and O(1) Space

--- Math&Patter&Recursion/Math/EasyMaths/code/closestNumber.py
+++ Math&Patter&Recursion/Math/EasyMaths/code/closestNumber.py
@@ -20,12 +20,31 @@ def closestNumber1(n, m):
                 min_diff = diff
     return closest
 
-### Method 2:
+### Method 2: [Expected Approach] By finding Quotient - O(1) Time and O(1) Space
+def closestNumber2(n, m):
+    # find Quotient
+    q = int (n / m)
 
+    # 1st possible closest number
+    n1 = m * q
+
+    # 2nd possible closest number
+    if ((n*m) > 0):
+        n2 = (m * (q + 1))
+    else: 
+        n2 = (m * (q - 1))
+
+    # if true, then n1 is the required closest number
+    if (abs(n - n1) < abs(n - n2)):
+        return n1
+    
+    # else n2 is the required closest number
+    return n2
 
 # Driver code
 
 if __name__ == "__main__":
     n = 10
     m = 6
-    print(f"The closest number to {n} that is divisible by {m} is: {closestNumber1(n = n, m = m)}")
+    print(f"Method 1 - The closest number to {n} that is divisible by {m} is: {closestNumber1(n = n, m = m)}")
+    print(f"Method 2 - The closest number to {-15} that is divisible by {m} is: {closestNumber2(n = -15, m = m)}")
