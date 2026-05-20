87c81343c03dc60a075e1835431c839ea68d161a
Me_Not404 <m3not404@gmail.com>
2026-05-20 16:54:41 +0000

    code(Sum of Consecutive): [Expected Approach] Using Power of 2 Property

--- Math&Patter&Recursion/Math/EasyMaths/code/isSumOfConsecutive.py
+++ Math&Patter&Recursion/Math/EasyMaths/code/isSumOfConsecutive.py
@@ -5,7 +5,7 @@
 #__________________________________________________________________
 
 
-## Method 1 [Naive Approach]: Checking All Consecutive Sums
+## Method 1: [Naive Approach] Checking All Consecutive Sums
 """
 #  Time Complexity: O(n2)
 # Space complexity: O(1)
@@ -33,12 +33,36 @@ def isSumOfConsecutive1(n):
         return False
 
 
+## Method 2: [Expected Approach] Using Power of 2 Property
+"""
+#  Time Complexity: O(1)
+# Space complexity: O(1)
+
+"
+The idea is that a number can be written as the sum of consecutive positive numbers if it is not a power of 2. So, we simply check whether n is a power of 2 using bit manipulation.
+"
+"""
+def isSumOfConsecutive2(n):
+    
+    # 1 can't be represented
+    if n == 1:
+        return False
+    
+    # Check if n is power of 2
+    if(n & (n - 1)) == 0:
+        return False
+    return True
 
 
 # Driver Code
 if __name__ == "__main__":
     n = 8
     if isSumOfConsecutive1(n = n):
+        print(f"{n} can be expressed as the sum of two or more consecutive numbers.")
+    else:
+        print(f"{n} cannot be expressed as the sum of two or more consecutive numbers.")
+
+    if isSumOfConsecutive2(n = n):
         print(f"{n} can be expressed as the sum of two or more consecutive numbers.")
     else:
         print(f"{n} cannot be expressed as the sum of two or more consecutive numbers.")
