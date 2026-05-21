aa7306fd0715744ed9735c3c9d594e887f7fcd55
Me_Not404 <m3not404@gmail.com>
2026-05-21 14:30:55 +0000

    code(Medium Pattern): Printing Pyramid Patterns

---
+++ Math&Patter&Recursion/Pattern/MediumPattern/code/pyramid.py
@@ -0,0 +1,31 @@
+
+#______________________________
+#
+#   Medium Pattern --- Pyramid
+#______________________________
+
+
+"""
+#  Time Complexity: O(n2)
+# Space complexity: O(1)
+
+"
+The idea is to use two nested loops. The outer loop is used to track the rows, while the inner loops are used to print the required spaces and stars for each row.
+"
+"""
+
+def pyramid(n):
+
+    # Outer loop to handle number of rows
+    for i in range(1, n + 1):
+
+        # printing spaces
+        print(" "*(n - i), end="")
+        # printing stars
+        print("*" * ( 2 * i - 1))
+
+# Drive code
+if __name__ == '__main__':
+    n = 5
+    if n > 1:
+        pyramid(n=n)
