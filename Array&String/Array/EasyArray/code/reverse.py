3f4c97c500f3814eb1e122c62deb5ada5a0f42f6
Me_Not404 <m3not404@gmail.com>
2026-06-01 14:39:14 +0000

    code(Array Reverse): [Expected Approach - 1] Using Two Pointers

--- Array&String/Array/EasyArray/code/reverse.py
+++ Array&String/Array/EasyArray/code/reverse.py
@@ -12,7 +12,7 @@ Now, copy all elements from original array to the temporary array in reverse ord
 Finally, copy all the elements from temporary array back to the original array.
 
     # O(n) Complexity Time
-    # O(1) Complexity Space
+    # O(n) Complexity Space
 """
 
 def reverseArrayTemp(arr):
@@ -30,12 +30,50 @@ def reverseArrayTemp(arr):
         arr[i] = temp[i]
 
 
+# Method 2: Using Two Pointers
+"""
+TThe idea is to maintain two pointers: left and right, such that left points at the beginning of the array and right points to the end of the array.
+While left pointer is less than the right pointer, swap the elements at these two positions. After each swap, increment the left pointer and decrement the right pointer to move towards the center of array. This will swap all the elements in the first half with their corresponding element in the second half.
+    # O(n) Complexity Time
+    # O(1) Complexity Space
+"""
+def reverseArray2Pointers(arr):
+    n = len(arr)
+
+    # Initialize left to the beginning
+    # and right to the end of the array
+    left = 0
+    right = n - 1
+
+    # Iterate till left is less than right
+    while left < right :
+
+        # Swap the elements at left
+        # and right position
+        arr[left], arr[right] = arr[right], arr[left]
+
+        # Increment left pointer
+        left += 1
+        # Decrement right pointer
+        right -= 1
+
 # Driver Code
 if __name__ == "__main__":
-    arr = [1, 4, 3, 2, 6, 5]
-    print(f"Original array: {arr}")
-    reverseArrayTemp(arr)
-    print("[Naive Approach] Using a temporary array - Array Reverse: ")
-    for i in range(len(arr)):
-        print(arr[i], end=" ")
+
+    # Method 1: Using a temporary array
+    arr1 = [1, 4, 3, 2, 6, 5]
+    print(f"Original array: {arr1}")
+    reverseArrayTemp(arr1)
+    print("[Naive Approach] Using a temporary array - Array Reverse: ", end="")
+    for i in range(len(arr1)):
+        print(arr1[i], end=" ")
+    print("\n")
+
+    # Method 2: Using Two Pointers
+    arr2 = [8, 2, 12, 6, 24, 4, 14]
+    print(f"Original array: {arr2}")
+    reverseArray2Pointers(arr2)
+    print("[Expected Approach - 1] Using Two Pointers - O(n) Time and O(1) Space: ", end="")
+    for i in range(len(arr2)):
+        print(arr2[i], end=" ")
     print()
