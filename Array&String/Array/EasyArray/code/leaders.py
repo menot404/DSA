adb5e652b136e0daf6e9862c454838f9dce22ec5
Me_Not404 <m3not404@gmail.com>
2026-06-10 14:49:27 +0000

    doc(Leaders in an array): [Expected Approach] Using Suffix Maximum

--- Array&String/Array/EasyArray/code/leaders.py
+++ Array&String/Array/EasyArray/code/leaders.py
@@ -25,7 +25,43 @@ def leadersLoop(arr):
             result.append(arr[i])
     return result
 
+
+# Method 2: [Expected Approach] Using Suffix Maximum
+"""
+The idea is to scan all the elements from right to left in an array and keep track of the maximum till now. 
+When the maximum changes its value, add it to the result. 
+Finally reverse the result  
+    # O(n) Time
+    # O(1) Space
+"""
+def leadersSuffixMax(arr):
+    result = []
+    n = len(arr)
+
+    ## Start with the rightmost element
+    maxRight = arr[-1]
+
+    # Rightmost element is always a leader
+    result.append(maxRight)
+
+    # Traverse the array from right to left
+    for i in range(n - 2, -1, -1):
+        if arr[i] >= maxRight:
+            maxRight = arr[i]
+            result.append(maxRight)
+
+    # Reverse the result list to maintain
+    # original order
+    result.reverse()
+    return result
+
 if __name__ == "__main__":
     arr = [16, 17, 4, 3, 5, 2]
     result = leadersLoop(arr=arr)
+    print(" [Naive Approach] Using Nested Loops: ", end='')
+    print(' '.join(map(str, result))) 
+
+    arr  = [1, 8, 18, 8, 26, 0, 12, 1]
+    result = leadersSuffixMax(arr=arr) ## Update Result 
+    print("[Expected Approach] Using Suffix Maximum: ", end='')
     print(' '.join(map(str, result))) 
